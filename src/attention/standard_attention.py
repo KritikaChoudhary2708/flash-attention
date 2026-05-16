9b877b24ffeda8a06033d5ba918a9a4e1a5f7480
KritikaChoudhary2708 <kritikachoudhary2708@gmail.com>
2026-05-17 02:52:21 +0530

    added basic structure

---
+++ src/attention/standard_attention.py
@@ -0,0 +1,12 @@
+import mlx.core as mx
+import math
+
+def standard_attention(Q,K,V, mask =None):
+          d_k =  Q.shape[-1]
+          scale = 1.0 / math.sqrt(d_k)
+          scores = mx.matmul(q, K.transpose(0,1,3,2))*scale
+          if mask is not None:
+                    scores = scores+ mask
+          weights = mx.softmax(scores, axis =-1)
+          output = mx.matmul(weights, V)
+          return output
